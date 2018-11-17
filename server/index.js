3d2a656710e6cf784d206905614289ca069ca35a
slieberman1126 <slieberman1126@gmail.com>
2018-11-17 13:29:53 -0500

    listening on port

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
