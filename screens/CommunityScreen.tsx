d12b99adec8742445fbc73549191b533f2a71502
briantam23 <briantam23@gmail.com>
2018-11-24 16:12:31 -0500

    initialized navigation link

--- screens/CommunityScreen.tsx
+++ screens/CommunityScreen.tsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { OpenMapDirections } from 'react-native-navigation-directions';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-// import Community from '../components/Community';
+export default class Directions extends Component {
+ _callShowDirections = () => {
+    const startPoint = {
+      longitude: -8.945406,
+      latitude: 38.575078
+    } 
 
-export default class CommunityScreen extends Component {
-  static navigationOptions = { title: 'Community' };
+    const endPoint = {
+      longitude: -8.9454275,
+      latitude: 38.5722429
+    }
 
+		const transportPlan = 'w';
+
+    OpenMapDirections(startPoint, endPoint, transportPlan).then(res => {
+      console.log(res)
+    });
+  }
+  
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* <Community/> */}
-      </ScrollView>
+      <View style={styles.container}>
+        <Text>Show direction between two random points!</Text>
+        <Button
+        onPress={() => { this._callShowDirections() }}
+        title="Open map"
+        color="#841584"
+      />
+      </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   container: {
