d14d6aacb525639d18ce3980a696c65dc57dc36b
slieberman1126 <slieberman1126@gmail.com>
2018-11-25 21:36:19 -0500

    into typescript

--- screens/PictureScreen.tsx
+++ screens/PictureScreen.tsx
@@ -1,65 +1,105 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import { Camera, Permissions } from 'expo';
-export default class PictureScreen extends Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  };
-  async componentDidMount() {
+import React from 'react';
+import { Component } from 'react';
+import { Permissions } from 'expo';
+import { Text } from 'react-native';
+import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Camera } from 'expo';
+import { Alert } from 'react-native';
+
+interface State {
+  hasCameraPermission: boolean | undefined;
+  type: string;
+  uri: string;
+}
+
+export default class PictureScreen extends Component<{}, State> {
+  constructor(props: {}, context?: any) {
+    super(props, context);
+    this.state = {
+      hasCameraPermission: undefined,
+      type: Camera.Constants.Type.back,
+      uri: 'empty',
+    };
+    this.takePic = this.takePic.bind(this);
+  }
+  public async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
   }
-  render() {
+
+  public render() {
     const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}
-            >
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  });
-                }}
-              >
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
-                >
-                  {' '}
-                  Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
+
+    if (hasCameraPermission === undefined) {
+      return <Text>Get permission to Camera</Text>;
     }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={{ flex: 1 }}
+          type={this.state.type}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+            }}
+          >
+            <TouchableOpacity
+              style={styles.snapButton}
+              onPress={() => this.takePic()}
+            >
+              <Text style={styles.snapText}> Capture </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
+  private toggleCameraType() {
+    this.setState({
+      type: this.state.type === 'back' ? 'front' : 'back',
+    });
+    Alert.alert('Flipped');
+  }
+  private async takePic() {
+    if (this.camera) {
+      await this.camera
+        .takePictureAsync()
+        .then(picture => this.setState({ uri: picture.uri }));
+    }
+    Alert.alert('Picture taken');
   }
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  snapText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  snapButton: {
+    backgroundColor: 'dodgerblue',
+    flex: 0.5,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 50,
+    marginTop: 20,
+    borderRadius: 8,
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
