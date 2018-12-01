4f12283f4a082eb1157ed7508a6305a4ba000736
Your Name <fernandez.Leovanny@gmail.com>
2018-11-30 21:45:51 -0500

    adding firebase and connecting signup to firebase

---
+++ screens/signUpScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SignUp from '../components/SignUp';
+
+
+export default class signUpScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Sign Up',
+  };
+
+  render() {
+    /* Go ahead and delete ExpoConfigView and replace it with your
+     * content, we just wanted to give you a quick view of your config */
+    return <SignUp />;
+  }
+}
