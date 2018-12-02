ae79329e494b6d786aea4a01fb1b8b2cf031e270
Your Name <fernandez.Leovanny@gmail.com>
2018-12-01 21:14:17 -0500

    rendering profile and file structure change

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,59 +1,123 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {AppRegistry, View, TextInput, Text} from 'react-native';
-import {FormLabel, FormInput, FormValidationMessage, Button, Form} from 'react-native-elements';
-import firebase from 'firebase';
+import {FormLabel, FormInput, FormValidationMessage, Button, Form, Avatar, Card} from 'react-native-elements';
+import firebase from '../firebase';
 
+const db = firebase.firestore();
 
 class SignUp extends Component {
   constructor(props) {
     super(props)
-    this.state=({
-      firstName:'',
-      lastName:'',
+    this.state={
       userName:'',
       email:'',
-      password:''
-    })
+      password:'',
+      points: 0,
+      loggedIn:false,
+    }
   }
-  signUp = (email,password) => {
+  signUp = (email,password,userName) => {
         try{
           firebase.auth().createUserWithEmailAndPassword(email,password)
+          .then(user => db.collection("User").add({
+             userName:userName,
+             email:email,
+             password:password,
+             id:`${user.user.uid}`
+            }));
+          this.clear();
         }
         catch(error){
           console.log(error.toString())
         }
       }
+
+  clear = ()=> {
+    this.inputOne.clearText();
+    this.inputTwo.clearText();
+    this.inputThree.clearText();
+    this.setState({
+      loggedIn:true,
+    })
+  }
+
+  logOut = () => {
+    try{
+      firebase.auth().signOut()
+      this.setState({
+        userName:'',
+        email:'',
+        password:'',
+        points: 0,
+        loggedIn:false,
+      })
+    }
+    catch(error) {
+      console.log(error.toString())
+    }
+  }
+
+  logIn=(email,password) => {
+    firebase.auth.signInWithEmailAndPassword(email, password)
+  }
+
   render() {
     return (
-    <View>
-      <FormLabel>First Name</FormLabel>
-      <FormInput
-      onChangeText={(firstName) => this.setState({firstName})}
-      />
-      <FormLabel>Last Name</FormLabel>
-      <FormInput
-      onChangeText={(lastName) => this.setState({lastName})}
-      />
-      <FormLabel>User Name</FormLabel>
-      <FormInput
-      onChangeText={(userName) => this.setState({userName})}
-      />
-      <FormLabel>Email</FormLabel>
-      <FormInput
-        onChangeText={(email) => this.setState({email})}
-      />
-      <FormLabel>Password</FormLabel>
-      <FormInput
-        onChangeText={(password) => this.setState({password})}
-      />
-      <Button
-        raised
-        backgroundColor='#3E9428'
-        title='Sign Up'
-        onPress={() => this.signUp(this.state.email, this.state.password)}
+      <View>
+      {this.state.loggedIn === false ?
+      <View>
+        <FormLabel>User Name</FormLabel>
+        <FormInput
+        onChangeText={(userName) => this.setState({userName})}
+        ref={inputOne => this.inputOne = inputOne}
         />
-      </View>
+        <FormLabel>Email</FormLabel>
+        <FormInput
+          onChangeText={(email) => this.setState({email})}
+          ref={inputTwo => this.inputTwo = inputTwo}
+        />
+        <FormLabel>Password</FormLabel>
+        <FormInput
+          onChangeText={(password) => this.setState({password})}
+          ref={inputThree => this.inputThree = inputThree}
+        />
+        <Button
+          raised
+          backgroundColor='#3E9428'
+          title='Sign Up'
+          onPress={() => this.signUp(this.state.email, this.state.password, this.state.userName)}
+          />
+          <Button
+            style={{marginTop:10}}
+            raised
+            backgroundColor='#3E9428'
+            title='Log In'
+            onPress={() => this.logIn(this.state.email, this.state.password)}
+            />
+        </View>
+        :
+        <View>
+        <Card containerStyle={{marginTop:150}}>
+
+          <Avatar
+          xlarge
+          title={this.state.userName[0]}
+          containerStyle={{ marginLeft: 100, marginRight:115}}
+          rounded
+           />
+           <Text style={{marginTop:25, marginLeft:110, marginRight:75, fontSize:40}}>{this.state.userName}</Text>
+           <Text style={{marginTop:10, marginLeft:110, marginRight:75, fontSize:35}}>Points: {this.state.points}</Text>
+           <Button
+           style={{marginTop:10}}
+           raised
+           backgroundColor='#3E9428'
+           title='Log Out'
+           onPress={()=>this.logOut()}
+           />
+           </Card>
+        </View>}
+        </View>
     )
   }
 }
